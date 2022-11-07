008f645b03f92b53be3927c09935f46f42f35a47
brahim abouihia <abouihia.brahim@gmail.com>
2022-11-07 13:17:38 +0100

    Completed module

--- src/main/java/com/pluralsight/blog/model/Author.java
+++ src/main/java/com/pluralsight/blog/model/Author.java
@@ -18,11 +18,18 @@ public class Author {
     private Long id;
     private String firstname;
     private String lastname;
+    @JsonIgnore
     private String username;
+    @JsonIgnore
     private String password;
+    @OneToMany
+    private List<Post> posts;
+
+
 
     public Author() {
         super();
+        this.posts = new ArrayList<>();
     }
 
     public Author(String username, String firstname, String lastname, String password) {
@@ -85,10 +92,16 @@ public class Author {
     }
 
     public List<Post> getPosts() {
-        return null;
+        return this.posts;
     }
 
     public void addPost(Post post) {
-        return;
+        this.posts.add(post);
     }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+
 }
